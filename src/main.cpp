b1adca72da6579c37eff8ceaf9b307566c3d40ba
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-04-12 15:42:21 +0300

    View is Ready

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,59 @@
 #include <vector>
 #include <random>
 
+#include <SFML/Graphics.hpp>
 #include "board_model.h"
 
 using namespace std;
-
+using namespace sf;
 
 int main() {
 
-	Board board(10, 10, 1);
+	// unsigned width = 854; unsigned height = 480;
+	unsigned width = 200; unsigned height = 100;
 
-	for (int i = 0; i < 10; i++) {
-		board.step();
+	unsigned cell_num = height * width;
+
+	Board board(width, height, 5);
+
+	RenderWindow window(VideoMode(width, height), "Langton's Ant");
+	// window.setVerticalSyncEnabled(true);
+	// window.setFramerateLimit(300);
+
+	Texture texture;
+	if (!texture.create(width, height)) { exit(1); }
+	Sprite sprite;
+
+	uint8_t *pixels = new uint8_t[cell_num * 4];
+	if (!pixels) {
+		std::cout << "Malloc " << cell_num * sizeof(uint8_t) << " failed!\n";
+		exit(1);
 	}
+
+	while (window.isOpen()) {
+
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+		}
+
+		board.step();
+
+		for (size_t i = 0; i < cell_num; ++i) {
+			((uint32_t*)pixels)[i] = board[i] ? 0xFFFFFFFF : 0xFF000000;
+		}
+
+		texture.update(pixels);
+		sprite.setTexture(texture);
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+
+	delete[] pixels;
 
 	return 0;
 }
